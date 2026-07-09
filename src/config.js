4a283fa587b7b599776fa06a42891498ffc38c60
Lumo Dev <lumobissau@gmail.com>
2026-07-09 02:10:13 +0100

    Bypass Netlify proxy and fetch Render directly

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_URL = process.env.NODE_ENV === 'production'
-    ? "/api"
+    ? "https://lumo-backend-api.onrender.com/api"
     : (() => {
         const hostname = window.location.hostname;
         return `http://${hostname}:5000/api`;
